a4126df049d9971189172c7daef335333dabe54b
Rocio Aramberri <rocio@opencraft.com>
2018-09-08 18:40:00 -0300

    Add one more test and url reverse

--- backend/risks/test_api.py
+++ backend/risks/test_api.py
@@ -20,6 +20,7 @@ class TestRisksAPI():
         """
         self.factory = APIRequestFactory()
         self.view = RiskTypeViewSet.as_view({'get': 'list'})
+        self.viewDetail = RiskTypeViewSet.as_view({'get': 'retrieve'})
         self.client = APIClient()
 
     def test_empty_risk_type_request(self):
@@ -34,10 +35,10 @@ class TestRisksAPI():
         """
         Tests adding a new risk_type.
         """
-        self.client.post('/risk_type/', {'name': 'Automobile'}, format='json')
-        self.client.post('/field_type/', {'name': 'text'}, format='json')
+        self.client.post(reverse('risks:risktype-list'), {'name': 'Automobile'}, format='json')
+        self.client.post(reverse('risks:fieldtype-list'), {'name': 'text'}, format='json')
         self.client.post(
-            '/field/',
+            reverse('risks:field-list'),
             {
                 'name': 'brand',
                 'risk_type': 1,
@@ -45,7 +46,7 @@ class TestRisksAPI():
             },
             format='json'
         )
-        request = self.factory.get('/risk_type/', format='json')
+        request = self.factory.get(reverse('risks:risktype-list'), format='json')
         response = self.view(request)
         response.render()
         assert json.loads(response.content) == [
@@ -62,3 +63,14 @@ class TestRisksAPI():
             }
         ]
         assert response.status_code == 200
+
+    def test_risk_type_get(self):
+        self.client.post(reverse('risks:risktype-list'), {'name': 'Automobile'}, format='json')
+        request = self.factory.get(reverse('risks:risktype-detail', kwargs={'pk': 2}), format='json')
+        response = self.viewDetail(request, pk='2')
+        response.render()
+        assert json.loads(response.content) == {
+            'fields': [],
+            'id': 2,
+            'name': 'Automobile'
+        }
