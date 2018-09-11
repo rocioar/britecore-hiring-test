3db92285878658e4e4c507c92eaadf6f09db4a2e
Rocio Aramberri <rocio@opencraft.com>
2018-09-11 12:46:09 -0300

    Add schema using coreapi

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,13 +1,18 @@
+from rest_framework.schemas import get_schema_view
+
 from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
+
+schema_view = get_schema_view(title='Risks API')
 
 
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     path(r'', include(('britecorehiringtest.risks.urls', 'risks'), namespace='risks')),
+    path(r'schema/', schema_view),
     # Your stuff: custom urls includes go here
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
